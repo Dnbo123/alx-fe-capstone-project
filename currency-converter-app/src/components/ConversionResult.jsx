773917b14a02564bf1ab3f648e1c1ef124b308cc
Don Bosco <don965877@gmail.com>
2024-10-02 03:22:15 +0300

    added css to conversion component

--- currency-converter-app/src/components/ConversionResult.jsx
+++ currency-converter-app/src/components/ConversionResult.jsx
@@ -2,12 +2,12 @@
 
 const ConversionResult =() => {
   return (
-    <div>
-      <h2>Results for Conversion</h2>
-      <p>
+    <div className="bg-gray-100 p-4 rounded-md">
+      <h2 className="text-lg font-semibold mb-2">Results for Conversion</h2>
+      <p  className="text-gray-700">
         {amount} {fromCurrency} =
       </p>
-      <p>
+      <p className="text-2xl font-bold text-green-600">
         {convertedAmount} {toCurrency}
       </p>
     </div>
