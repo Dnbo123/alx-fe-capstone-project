dbe994d5e8d7967f54e00c939e41c6499b63cdef
Don Bosco <don965877@gmail.com>
2024-10-02 03:09:58 +0300

    created label input for entering amount to be converted

--- currency-converter-app/src/components/AmountInput.jsx
+++ currency-converter-app/src/components/AmountInput.jsx
@@ -1,11 +1,23 @@
 
 
-function AmountInput() {
-  return (
-<>
+const AmountInput =({value, onChange}) =>{
 
-</>
-  )
-}
+  return (
+<div className="flex flex-col">
+<label htmlFor="amount" className="mb-2 font-medium text-gray-700">
+  Enter Amount...
+</label>
+<input 
+   id="amount"
+   type="number"
+   value={value}
+   onChange={(e) => onChange(parseFloat(e.target.value))}
+   min="0"
+   step="0.01"
+   className="p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"/>
+
+</div>
+  );
+};
 
 export default AmountInput
